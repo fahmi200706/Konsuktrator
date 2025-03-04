5762465dff8dd6f3de00380e3f273b1dd0d925fa
fahmi200706 <fahmi.asyahdan@gmail.com>
2025-03-04 11:23:28 +0700

    Add files via upload
    commit konstruktor

---
+++ A.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konstrukator;
+
+/**
+ *
+ * @author DELL
+ */
+public class A {
+    A(String param1){
+        System.out.println("KOnstruktor class A dengan Parameter " + param1 +"dieksekusi.....");
+    } 
+}
